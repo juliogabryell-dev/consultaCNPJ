4a89033d3eccab4e1ce83e7dfb8a3c2fa4d34bc1
Julio Gabryell Rodrigues <julio.caetano@dellys.com.br>
2025-09-20 17:57:41 -0300

    Primeiro commit!

---
+++ ConsultaCNPJ.dpr
@@ -0,0 +1,17 @@
+program ConsultaCNPJ;
+
+uses
+  Vcl.Forms,
+  uPrincipal in 'uPrincipal.pas' {frmPrincipal},
+  Vcl.Themes,
+  Vcl.Styles;
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  TStyleManager.TrySetStyle('Windows10');
+  Application.CreateForm(TfrmPrincipal, frmPrincipal);
+  Application.Run;
+end.
